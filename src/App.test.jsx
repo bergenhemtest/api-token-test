6d02f4a33b60730ebfe41be9ae8ad801816b72a3
Carl Bergenhem <cbergenhem@gmail.com>
2022-11-28 15:44:57 -0500

    Make label for first group of tests more understandable

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest';
 import { render, screen } from '@solidjs/testing-library';
 import App from './App';
 
-describe('App component', () => {
+describe('App Component Tests', () => {
   it('should render a link to learn solid', () => {
     render(() => <App />);
     const learnElement = screen.getByText('Learn Solid');
