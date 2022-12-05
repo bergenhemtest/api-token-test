396f1bc56f5a0d72158a5b57d10aa188099cfd5b
Carl Bergenhem <cbergenhem@gmail.com>
2022-12-05 14:22:11 -0500

    Ensure Sentry errors do not trigger in local dev

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,32 @@ function App() {
 
   onMount(() => {
     console.log("App mounted");
-    Sentry.init({
-      dsn: "https://979354779c2640c2b9464b4e22046c49@o4504176326279168.ingest.sentry.io/4504244182450176",
-      integrations: [new BrowserTracing()],
     
-      // Set tracesSampleRate to 1.0 to capture 100%
-      // of transactions for performance monitoring.
-      // We recommend adjusting this value in production
-      tracesSampleRate: 1.0,
-    });
     const envVar = import.meta.env.VITE_ENVIRONMENT;
+    
+    if(envVar) { //will be undefined in local development
+      Sentry.init({
+        dsn: "https://979354779c2640c2b9464b4e22046c49@o4504176326279168.ingest.sentry.io/4504244182450176",
+        integrations: [new BrowserTracing()],
+      
+        // Set tracesSampleRate to 1.0 to capture 100%
+        // of transactions for performance monitoring.
+        // We recommend adjusting this value in production
+        tracesSampleRate: 1.0,
+      });
+    }
+    
+    let faviconLink = document.querySelector('link[rel="shortcut icon"]') ||
+    document.querySelector('link[rel="icon"]');
+
+    if(!faviconLink) {
+      faviconLink = document.createElement('link');
+      link.id = "favicon";
+      link.rel = "shortcut icon";
+      document.head.appendChild(link);
+    }
     console.log("Environment variable", envVar);
+    console.log("Favicon", faviconLink);
   });
 
   const [errorStart, setErrorStart] = createSignal(null);
